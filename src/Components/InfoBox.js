f6a3708e3757baef462532a1fcd784a988ff394a
ppanwar <ppanwar@cholachariots.com>
2024-02-26 18:55:05 +0530

    new update

--- src/Components/InfoBox.js
+++ src/Components/InfoBox.js
@@ -7,13 +7,16 @@ const InfoBox = () => {
       <div className="info-content w-full md:w-[90%] lg:w-[70%] xl:w-[60%] 2xl:w-[50%] flex flex-col md:flex-row items-center md:items-start justify-center md:justify-start">
      
         <div className="text-container md:w-3/5 lg:w-2/3 xl:w-3/4 2xl:w-1/2 text-center md:text-left mb-4 md:mb-0 order-1 md:order-2">
-          <h2 className="text-[22px] text-[#2080d1] font-bold mt-4 md:mt-0">Our Newest Moisture Analyzer, the MAC116</h2>
-          <p className="text-gray-500 mt-2 md:mt-4 text-[17px] leading-relaxed">Specifically designed for use on continuous conveyor ovens that are used to cook meat and poultry, the MAC116 is successfully operating in a large number of cooking installations in many countries throughout the world.</p>
+          <h2 className="text-[22px] lg:w-[500px] md:w-[500px] text-[#2080d1] font-bold mt-4 md:mt-0">Our Newest Moisture Analyzer, the MAC116</h2>
+          <p className="text-gray-500 md:w-[500px] lg:w-[500px] mt-2 md:mt-4 text-[17px] leading-relaxed">Specifically designed for use on continuous conveyor ovens that are used to cook meat and poultry, the MAC116 is successfully operating in a large number of cooking installations in many countries throughout the world.</p>
           <button className="px-14 py-4 bg-[#2080d1] text-white mt-4">Learn more</button>
         
         </div>
-        <img src={im} alt="Info" className="order-2 md:order-1 w-full md:w-2/5 lg:w-1/3 xl:w-1/4 h-auto md:h-60 md:mr-8 mt-8 md:mt-0" />
+       
       </div>
+      <img src={im} alt="Info" className="order-2 md:order-1 
+        w-full md:w-2/5 lg:w-1/4 xl:w-1/5 lg:mr-40 h-auto
+         md:h-60 md:mr-8 mt-8 md:mt-0 " />
     </div>
   );
 };
